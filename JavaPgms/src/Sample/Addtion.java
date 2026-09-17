0c6e67fc50eddd9cdbbabab5013a545555b571f7
FI-THARUNKUMAR <FI-THARUNKUMAR@FI-FUNDS>
2026-09-18 02:28:14 +0530

    First Commit

---
+++ JavaPgms/src/Sample/Addtion.java
@@ -0,0 +1,13 @@
+package Sample;
+
+public class Addtion {
+	
+	public static void main(String[] args) {
+		//system.out.print("Hi");
+		System.out.println("Hello");
+		System.out.println("false");
+		System.out.println("kumar");
+	}		
+		
+
+}
---
+++ JavaPgms/src/Sample/UserInput.java
@@ -0,0 +1,22 @@
+package Sample;
+
+import java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number");
+		int num = input.nextInt();
+		System.out.println(num);
+		input.close();
+		
+		
+      
+	}
+	
+
+}
