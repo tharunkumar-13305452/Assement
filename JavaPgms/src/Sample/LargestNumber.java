8d5fccf1ac408273c7df0eb9dd64be1dc3936a5d
FI-THARUNKUMAR <FI-THARUNKUMAR@FI-FUNDS>
2026-09-19 12:12:56 +0530

    Frist Commit

---
+++ JavaPgms/src/Sample/LargestNumber.java
@@ -0,0 +1,41 @@
+package Sample;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter frist number");
+		int a = sc.nextInt();
+		
+		System.out.print("Enter second number");
+		int b = sc.nextInt();
+		
+		System.out.print("Enter thrid number");
+		int c = sc.nextInt();
+		
+		if (a >= b && a > c) {
+			System.out.println("Largest number is " + a);
+		}
+		else if (b >= a && b >= c) {
+			System.out.println("Largest number is " + b);
+			
+		}
+		else {
+			System.out.println("Largest number is " + c);
+			
+		}
+		
+		sc.close();
+		
+	}
+	
+}
+		
+		
+			
+		
+		
+		
